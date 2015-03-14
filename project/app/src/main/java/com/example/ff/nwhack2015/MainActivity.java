ba549775c1144c85045c835b3ffae1486b21d0bb
ff <ff@aight>
2015-03-14 12:14:26 -0700

    Got the connection to my Mojio app working

--- project/app/src/main/java/com/example/ff/nwhack2015/MainActivity.java
+++ project/app/src/main/java/com/example/ff/nwhack2015/MainActivity.java
@@ -1,17 +1,42 @@
 package com.example.ff.nwhack2015;
 
+import android.accounts.AccountManager;
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.example.ff.nwhack2015.NetworkingTask;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends Activity {
+    private List<NameValuePair> getParams = new ArrayList<NameValuePair>(3);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getParams.add(new BasicNameValuePair("response_type","token"));
+        getParams.add(new BasicNameValuePair("client_id","b9550897-97f4-48c0-b06e-bcf12db55c0b"));
+        getParams.add(new BasicNameValuePair("redirect_uri","this.game.app://getToken/"));
+
+        String url = "https://api.moj.io/OAuth2Sandbox/authorize?";
+        String paramString = URLEncodedUtils.format(getParams, "utf-8");
+
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url+paramString));
+        startActivity(i);
+
     }
 
 
--- project/app/src/main/java/com/example/ff/nwhack2015/NetworkingTask.java
+++ project/app/src/main/java/com/example/ff/nwhack2015/NetworkingTask.java
@@ -5,6 +5,7 @@ package com.example.ff.nwhack2015;
  */
 
 import android.os.AsyncTask;
+import android.util.Log;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -12,9 +13,16 @@ import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.scheme.SocketFactory;
+import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.apache.http.impl.client.DefaultHttpClient;
 
+import java.io.InputStream;
+import java.security.KeyStore;
 import java.util.List;
+
 
 /**
  * Created by ff on 13/03/15.
@@ -33,8 +41,8 @@ public class NetworkingTask extends AsyncTask<String,Void,HttpResponse> {
         if (!urlStr[0].endsWith("?"))
             urlStr[0]+="?";
         String paramString = URLEncodedUtils.format(params, "utf-8");
+        System.out.println(urlStr[0] + paramString);
         HttpGet httpGet = new HttpGet(urlStr[0] + paramString);
-
         try {
             return httpClient.execute(httpGet);
         } catch (Exception e) {
@@ -42,4 +50,16 @@ public class NetworkingTask extends AsyncTask<String,Void,HttpResponse> {
             return null;
         }
     }
+    /*protected void onPostExecute(HttpResponse result) {
+        try {
+            InputStream is = result.getEntity().getContent();
+            int data = is.read();
+            while (data !=-1) {
+                System.out.print((char) data);
+                data = is.read();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }*/
 }
